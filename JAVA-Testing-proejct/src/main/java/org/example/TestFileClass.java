a5d2095e6d77d1679506584763adc99f0d0bac26
manukundloo-hmcts <Manukundloo.sinha@hmcts.net>
2023-03-13 12:33:00 +0000

    Adding java files

---
+++ JAVA-Testing-proejct/src/main/java/org/example/TestFileClass.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class TestFileClass {
+
+    public static void main(String[] args) {
+        System.out.println("Adding comment to remove in future");
+        System.out.println("This comment will persist in future");
+    }
+}
